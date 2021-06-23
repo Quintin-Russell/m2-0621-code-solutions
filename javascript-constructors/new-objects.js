ed36e931467bb273bdee52c453767376c9d07afc
Quintin Russell <quinn.j.russell@gmail.com>
2021-06-22 21:03:40 -0700

    completed JS-constructors

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,22 @@
+//do all ops on Object, Array, Function, String, Number, and Boolean
+//use new op and assign to var an__
+let anObject = new Object();
+let anArray = new Array();
+let anFunction = new Function();
+let anString = new String();
+let anNumber = new Number();
+let anBoolean = new Boolean();
+
+console.log("value of anObject", anObject)
+console.log("value of anArray", anArray)
+console.log("value of anFunction", anFunction)
+console.log("value of anString", anString)
+console.log("value of anNumber", anNumber)
+console.log("value of anBoolean", anBoolean)
+
+console.log("typeof anObject", typeof(anObject))
+console.log("typeof anArray", typeof(anArray))
+console.log("typeof anFunction", typeof(anFunction))
+console.log("typeof anString", typeof(anString))
+console.log("typeof anNumber", typeof(anNumber))
+console.log("typeof anBoolean", typeof(anBoolean))
