1aec8cd49efd7854735e57767987453720a6b30f
Quintin Russell <quinn.j.russell@gmail.com>
2021-06-24 19:58:58 -0700

    completed JS-bank

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -2,14 +2,38 @@
 function Account(number, holder){
   this.number = number;
   this.holder = holder;
-  this.deposit = function(amt) {
-    if (amt){
-      number += amt
-    }
-  };
-  this.withdraw = function(amt) {
-    if (amt) {
-      number -= amt;
-    }
-  };
+  this.transactions = [];
+  this.balance = 0;
 }
+
+Account.prototype.deposit = function (amt) {
+  if ((amt > 0) && ((amt % 1) === 0)) {
+    let trans = new Transaction('deposit', amt);
+    this.transactions.push(trans);
+    this.balance += amt;
+    this.number += amt
+    return true
+  } else {
+    return false
+  }
+};
+
+Account.prototype.withdraw = function (amt) {
+  if ((amt > 0) && ((amt % 1) === 0)) {
+    let trans = new Transaction('withdrawal', amt);
+    this.transactions.push(trans);
+    this.balance -= amt;
+    this.number -= amt;
+    return true
+  } else {
+    return false
+  }
+};
+
+Account.prototype.getBalance = function () {
+  return this.balance
+};
+
+/* const acc = new Account(1000, "john");
+acc.deposit(100);
+console.log(acc.number) */
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,37 @@
 /* exported Bank */
+function Bank() {
+  this.accounts = [];
+  this.nextAccountNumber = 1;
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if ((balance > 0) && (balance % 1 === 0)) {
+    let acc = new Account(balance, holder);
+    acc.nextAccountNumber = this.nextAccountNumber;
+    acc.balance = balance;
+    this.accounts.push(acc);
+    this.nextAccountNumber +=1;
+    return (this.nextAccountNumber - 1)
+  } else {
+    return null
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  let account = null;
+  for (let acc of this.accounts) {
+    if (acc.nextAccountNumber === number) {
+      account = acc;
+      break
+    };
+  };
+  return account
+};
+
+Bank.prototype.getTotalAssets = function() {
+  let total = 0;
+  for (let acc of this.accounts) {
+    total += acc.balance
+  }
+  return total;
+}
