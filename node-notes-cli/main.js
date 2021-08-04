09f218fcbba5709407726a41166dd3d22a0cd302
Quintin Russell <quinn.j.russell@gmail.com>
2021-08-03 20:05:24 -0700

    completed issue 3

--- node-notes-cli/main.js
+++ node-notes-cli/main.js
@@ -27,11 +27,22 @@ function create() {
   return data
 }
 
+function deleteEntry() {
+  delete dataJSON.notes[argument];
+  const data = JSON.stringify(dataJSON);
+  return data
+}
+
+
 //logic tree to determine which function to run
 if (command === 'read') {
   read()
 } else if (command === 'create'){
   fs.writeFile('data.json', create(), (err,data)=> {
+    if (err) throw err
+  })
+} else if (command === 'delete') {
+  fs.writeFile('data.json', deleteEntry(), (err, data) => {
     if (err) throw err
   })
 }
