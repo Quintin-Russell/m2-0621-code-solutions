c47ec2a7337bd47106c104197d4d957907a0e698
Quintin Russell <quinn.j.russell@gmail.com>
2021-08-11 20:55:15 -0700

    working

---
+++ express-post-json/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+app.listen('3000', () => 'server is listening on port 3000');
+
+let nextId = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const arr = [];
+  for (const gr in grades) {
+    arr.push(grades[gr]);
+  }
+  res.json(arr);
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  const gr = req.body;
+  gr.id = nextId;
+  grades[nextId] = gr;
+  nextId++;
+  res.send(grades);
+});
