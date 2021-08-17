aaa28f207f978e0cd4650770548e476c9c3dec29
Quintin Russell <quinn.j.russell@gmail.com>
2021-08-16 21:56:34 -0700

    working

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -1,14 +1,14 @@
 // issue 1: simple GET req --> sends 200 code and JSON array of objs
 const express = require('express');
 const dataJSON = require('./data.json');
-// const fs = require('fs');
+const fs = require('fs');
 // const { nextTick } = require('process');
 const app = express();
 app.listen(3000, () => 'server is listening on port 3000');
 
-// function writeFile(file, json) {
-//   fs.writeFile(file, json, (err, data) => { if (err) throw err; });
-// }
+function writeFile(file, json) {
+  fs.writeFile(file, JSON.stringify(json, null, 2), (err, data) => { if (err) throw err; });
+}
 
 app.get('/api/notes', (req, res, next) => {
   const arr = [];
@@ -19,6 +19,8 @@ app.get('/api/notes', (req, res, next) => {
   res.status(200).json(arr);
   next();
 });
+
+app.use(express.json());
 
 // issue 2: GET req w/ id returns the obj with that id; negative id = 400 status+err;
 // id does not exist = 404status+err
@@ -52,21 +54,30 @@ app.get('/api/notes', (req, res, next) => {
 // no content = 400status + error obj; if content not successfully recorded in data.json
 // = 500status + error obj
 app.post('/api/notes', (req, res) => {
-  let nextId = dataJSON.nextId;
+  const nextId = dataJSON.nextId;
   // console.log(req);
   if (req.body !== undefined) {
-    const cont = req.body;
+    let cont = req.body;
+    cont = cont.content;
     const entry = {
-      id: nextId,
-      content: cont
+      content: cont,
+      id: nextId
     };
     dataJSON.notes[nextId] = entry;
-    nextId++;
+    dataJSON.nextId = nextId + 1;
+    writeFile('data.json', dataJSON);
     res.status(201).json(entry);
   } else {
     const err = {
       error: 'content is a required field'
     };
     res.status(400).json(err);
+  }
+}, (err, data) => {
+  if (err) {
+    const error = {
+      error: 'An unexpected error occurred'
+    };
+    escape.status(500).json(error);
   }
 });
