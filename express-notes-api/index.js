82c4492cf7e3a39ef2e909e14bb5e2922b8062b7
Quintin Russell <quinn.j.russell@gmail.com>
2021-08-17 21:29:53 -0700

    added delete and error handling

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -7,7 +7,11 @@ const app = express();
 app.listen(3000, () => 'server is listening on port 3000');
 
 function writeFile(file, json) {
-  fs.writeFile(file, JSON.stringify(json, null, 2), (err, data) => { if (err) throw err; });
+  fs.writeFile(file, JSON.stringify(json, null, 2), (err, data, next) => {
+    if (err) {
+      next(err);
+    }
+  });
 }
 
 app.get('/api/notes', (req, res, next) => {
@@ -24,11 +28,10 @@ app.use(express.json());
 
 // issue 2: GET req w/ id returns the obj with that id; negative id = 400 status+err;
 // id does not exist = 404status+err
-app.get('/api/notes', (req, res, next) => {
-  const id = req.body;
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
   let object = null;
   if (id < 0) {
-
     const message = {
       error: 'Please enter a valid ID'
     };
@@ -41,13 +44,11 @@ app.get('/api/notes', (req, res, next) => {
     }
   }
   if (object === null) {
-
     const message = {
       error: 'There is no note with that ID. Please enter a valid ID'
     };
     res.status(404).json(message);
   }
-  next();
 });
 
 // issue 3: POST req + content adds a new note obj to data.json + 201status;
@@ -55,8 +56,8 @@ app.get('/api/notes', (req, res, next) => {
 // = 500status + error obj
 app.post('/api/notes', (req, res) => {
   const nextId = dataJSON.nextId;
-  // console.log(req);
-  if (req.body !== undefined) {
+  const reqBody = req.body;
+  if ((Object.keys(reqBody).length) > 0) {
     let cont = req.body;
     cont = cont.content;
     const entry = {
@@ -66,18 +67,52 @@ app.post('/api/notes', (req, res) => {
     dataJSON.notes[nextId] = entry;
     dataJSON.nextId = nextId + 1;
     writeFile('data.json', dataJSON);
-    res.status(201).json(entry);
+    return res.status(201).json(entry);
   } else {
     const err = {
       error: 'content is a required field'
     };
-    res.status(400).json(err);
+    return res.status(400).json(err);
   }
-}, (err, data) => {
-  if (err) {
+});
+
+// issue 4: DELETE req:
+// xx- not a valid id(not a + integer): 400 status code + JSON error obj
+// xx- if +id but id doesnt exist: 404 status code + JSON error obj
+// xx- if valid id but error: next(err)
+// xxif valid id: delete note + 204 status
+
+app.delete('/api/notes/:id', (req, res, next) => {
+  const delId = req.params.id;
+  if (delId > 0) {
+    let entry;
+    for (const ent in dataJSON.notes) {
+      if (delId === ent) {
+        entry = dataJSON.notes[ent];
+        delete dataJSON.notes[ent];
+        writeFile('data.json', dataJSON);
+        res.status(204);
+        break;
+      }
+    }
+    if (entry === undefined) {
+      const error = {
+        error: `cannot find note with if ${delId}`
+      };
+      res.status(404).json(error);
+    }
+  } else {
     const error = {
-      error: 'An unexpected error occurred'
+      error: 'id must be a valid integer'
     };
-    escape.status(500).json(error);
+    res.status(400).json(error);
   }
+});
+
+app.use(function (err, req, res, next) {
+  const errorMes = {
+    error: 'An unexpected error occurred'
+  };
+  res.status(500).json(errorMes);
+  return err;
 });
